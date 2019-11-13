98647cff11a065cce0b1a83454988574c0693862
sayid <savyd.me@gmail.com>
2019-11-13 08:18:24 +0700

    learn store and state vuex

---
+++ src/components/Customer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="customer">
+    <h1>Hallo, mau beli TV?</h1>
+    <p>Sisa {{totalTVCount}} TV</p>
+    <p v-show="!totalTVCount">TV nya habis.. :(</p>
+    <button :disabled="!totalTVCount" @click="beliTV">Beli TV</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Customer",
+  computed: {
+    totalTVCount() {
+      return this.$store.state.totalTVCount;
+    }
+  },
+  methods: {
+    beliTV() {
+      this.$store.dispatch("hapusTV");
+    }
+  }
+};
+</script>
+
+<style scope>
+</style>
---
+++ src/store/store.js
@@ -0,0 +1,23 @@
+import Vuex from "vuex";
+import Vue from "vue";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  // state = gudang
+  state: {
+    totalTVCount: 10
+  },
+
+  getters: {
+    // buat mengambil property dr state
+  },
+
+  mutations: {
+    // jenny
+  },
+
+  actions: {
+    // larry
+  }
+});
