835a187f2682dd3cef9a1fdba9cefffd20e7da02
sayid <savyd.me@gmail.com>
2019-11-14 18:27:06 +0700

    finish leson vuex

--- src/components/Customer.vue
+++ src/components/Customer.vue
@@ -2,26 +2,46 @@
   <div class="customer">
     <h1>Hallo, mau beli TV?</h1>
     <p>Sisa {{totalTVCount}} TV</p>
+    <p v-show="happyStaff">Staff is Happing full!!</p>
     <p v-show="!totalTVCount">TV nya habis.. :(</p>
     <button :disabled="!totalTVCount" @click="beliTV">Beli TV</button>
+    <button :disabled="totalTVCount < 2" @click="beliDuaTV">Beli Dua TV</button>
   </div>
 </template>
 
 <script>
+import { mapState, mapGetters, mapActions } from "vuex";
 export default {
   name: "Customer",
   computed: {
-    totalTVCount() {
-      return this.$store.state.totalTVCount;
-    }
+    // Cara Lama
+    // totalTVCount() {
+    //   return this.$store.state.totalTVCount;
+    // },
+    // happyStaff() {
+    //   return this.$store.getters.happyStaff;
+    // }
+    // Cara Baru
+    ...mapState(["totalTVCount"]),
+    ...mapGetters(["happyStaff"])
   },
   methods: {
+    ...mapActions(["hapusTV"]),
     beliTV() {
-      this.$store.dispatch("hapusTV");
+      // this.$store.dispatch("hapusTV", 1);
+      this.hapusTV(1);
+    },
+    beliDuaTV() {
+      // this.$store.dispatch("hapusTV", 2);
+      this.hapusTV(2);
     }
   }
 };
 </script>
 
-<style scope>
+<style scoped>
+button {
+  /* display: block; */
+  margin: 1px 1px;
+}
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -6,18 +6,26 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   // state = gudang
   state: {
-    totalTVCount: 10
+    totalTVCount: 10,
+    wasLarryHappy: true,
+    wasJennyHappy: true
   },
 
   getters: {
     // buat mengambil property dr state
+    happyStaff: state => state.wasJennyHappy && state.wasJennyHappy
   },
 
   mutations: {
     // jenny
+    hapusTV: (state, jumlahTV) => state.totalTVCount -= jumlahTV
   },
 
   actions: {
     // larry
+    hapusTV: (context, jumlahTV) => {
+      if (context.state.totalTVCount >= jumlahTV)
+        context.commit("hapusTV", jumlahTV);
+    }
   }
 });
